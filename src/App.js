87a253ed149211b6af76439b9c8d3780c94a146e
Pushpendra Aneshwari <aneshwaripushpendra@gmail.com>
2022-06-07 14:28:15 +0530

    started building sidebar

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
 import Home from "./pages/Home";
 import "rsuite/dist/styles/rsuite-default.css";
+import { ProfileProvider } from "./context/profile.context";
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SignIn />
-      </PublicRoute>
+    <ProfileProvider>
+      <Switch>
+        <PublicRoute path="/signin">
+          <SignIn />
+        </PublicRoute>
 
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </ProfileProvider>
   );
 }
 
---
+++ src/components/dashboard/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div>bhai</div>;
+};
+
+export default Dashboard;
